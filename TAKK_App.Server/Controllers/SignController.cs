1dd904a7735c48019bdf4597a6a4e8ab09ba66fc
Ruben Riddarhaage <ruben_riddarhaage@hotmail.com>
2023-12-21 12:32:59 +0100

    SignController added

---
+++ TAKK_App.Server/Controllers/SignController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TAKK_App.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SignController : ControllerBase
+    {
+
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello World!");
+        }
+
+        [HttpPost]
+        public IActionResult Post(IFormFile file)
+        {
+            return Ok("Hello World!");
+        }
+    }
+}

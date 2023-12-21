abdba36ab777a3157ae1d06ee60dc4c91e2932c8
Ruben Riddarhaage <ruben_riddarhaage@hotmail.com>
2023-12-21 11:59:55 +0100

    Added properties to the Sign class

--- TAKK_App.Server/Models/Sign.cs
+++ TAKK_App.Server/Models/Sign.cs
@@ -1,8 +1,21 @@
-﻿namespace TAKK_App.Server.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TAKK_App.Server.Models
 {
     public class Sign
     {
-        
+        public int Id { get; set; }
 
+        [Required]
+        [StringLength(100)]
+        public string Title { get; set; }
+
+        [Required]
+        [Url]
+        [StringLength(500)]
+        public string ImgUrl { get; set; }
+
+        public string Category { get; set; }
+        public string Description { get; set; }
     }
 }

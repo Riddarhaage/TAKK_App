764c7d4744c76e9937229a2196a1ad4ed651bf90
Ruben Riddarhaage <ruben_riddarhaage@hotmail.com>
2024-01-03 11:50:35 +0100

    Added hamburger-menu icon to header. No functionality implemented yet.

--- takk_app.client/src/components/Header.jsx
+++ takk_app.client/src/components/Header.jsx
@@ -1,10 +1,12 @@
 import SearchInput from "./SearchInput";
 import CharacterFilter from "./CharacterFilter";
+import Hamburger from 'hamburger-react';
 
 function Header(props) {
     const {searchTerm ,setSearch,setCharFilter} = props;
 
     return <div className="header">
+        <Hamburger rounded size={24} duration={0.2} />
         <h1>Tecken</h1>
         <SearchInput searchTerm={searchTerm} setSearch={setSearch} />
         <CharacterFilter setCharFilter={setCharFilter} />

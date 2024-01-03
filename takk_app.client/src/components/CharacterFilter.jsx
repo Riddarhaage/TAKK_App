6c1bc835eb04ffe3d4777417e245efbe3ed86e9e
Ruben Riddarhaage <ruben_riddarhaage@hotmail.com>
2024-01-03 14:23:03 +0100

    Changed the character filter to be a drop-down menu instead.

--- takk_app.client/src/components/CharacterFilter.jsx
+++ takk_app.client/src/components/CharacterFilter.jsx
@@ -1,15 +1,23 @@
-﻿import React from 'react'
+﻿import React from 'react';
 
+//TODO: Finish styling this component
 function CharacterFilter(props) {
-    const {setCharFilter } = props;
+    const { setCharFilter } = props;
     const allchars = "ABCDEFGHIJKLMNOPQRSTUVWXYZÅÄÖ".split('');
 
+    const handleCharChange = (event) => {
+        setCharFilter(event.target.value);
+    };
+
     return (
-        <div>
-            <button className="charButtons" onClick={() => setCharFilter('#')}>0-9</button>
-            {allchars.map(char => (
-                <button className="charButtons" key={char} onClick={() => setCharFilter(char)}>{char}</button>
-            ))}
+        <div className="char-dropdown-container">
+            <select onChange={handleCharChange} className="charDropdown">
+                <option value="">Alla</option>
+                <option value="#">0-9</option>
+                {allchars.map(char => (
+                    <option key={char} value={char}>{char}</option>
+                ))}
+            </select>
         </div>
     );
 }

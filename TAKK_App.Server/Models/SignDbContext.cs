815730baf4a807c7228cd61d210e6217296a371b
Ruben Riddarhaage <ruben_riddarhaage@hotmail.com>
2024-01-09 15:36:07 +0100

    Added DbContext class and Entity Framework packages

---
+++ TAKK_App.Server/Models/SignDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TAKK_App.Server.Models
+{
+    public class SignDbContext : DbContext
+    {
+        public SignDbContext(DbContextOptions<SignDbContext> options) 
+            : base(options)
+        {
+        }
+    }
+}
